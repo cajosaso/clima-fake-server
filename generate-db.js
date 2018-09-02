10dc11080c5dec1c892d0575be1558718aa3f0cf
jisbruzzi <jose.sbru@gmail.com>
2018-09-02 17:00:59 -0300

    Agrego todas las ciudades al fake server[0]

--- generate-db.js
+++ generate-db.js
@@ -4,39 +4,33 @@ let fs=require("fs")
 let sleep = require("sleep")
 
 let db={}
+let badCities=[]
 normalRequest.get("http://localhost:3000/cities",function(error,response,body){
-    let cities=JSON.parse(body).filter((n)=>!n.includes(",AR") || n.includes("Buenos Aires")).slice(0,100)
+    let cities=JSON.parse(body)//.filter((n)=>!n.includes(",AR") || n.includes("Buenos Aires")).slice(0,100)
     db["cities"]=cities
     db["weather"]={}
     for (city of cities){
         console.log("Voy a traer:")
         console.log(city)
-        db["weather"][city]={}
-        let niceCity=encodeURIComponent(city)
-        /*
-        request.get("http://localhost:3000/weather/"+niceCity+"/current",function(e,r,b){
-            console.log(e)
-            console.log(r)
-            w[city]["current"]=b
-            console.log(w[city])
-        })
-        */
-        let res=request("GET","http://localhost:3000/weather/"+niceCity+"/current")
-        db["weather"][city]["current"]=JSON.parse(res.getBody("utf8"))
+        try{
+            db["weather"][city]={}
+            let niceCity=encodeURIComponent(city)
+            let res=request("GET","http://localhost:3000/weather/"+niceCity+"/current")
+            db["weather"][city]["current"]=JSON.parse(res.getBody("utf8"))
 
-        let res2=request("GET","http://localhost:3000/weather/"+niceCity+"/forecast")
-        db["weather"][city]["forecast"]=JSON.parse(res2.getBody("utf8"))
-        console.log(db)
-        /*
-        request.get("http://localhost:3000/weather/"+niceCity+"/forecast",function(e,r,b){
-            w[city]["forecast"]=b
-            console.log(w[city])
-        })
-        sleep.sleep(15)
-        */
-       console.log(Object.keys(db["weather"]).length,cities.length)
+            let res2=request("GET","http://localhost:3000/weather/"+niceCity+"/forecast")
+            db["weather"][city]["forecast"]=JSON.parse(res2.getBody("utf8"))
+            console.log(db)
+        }catch(e){
+            badCities.push(city)
+        }
+       
+        console.log(Object.keys(db["weather"]).length,cities.length)
+
+       fs.writeFileSync("./db.json",JSON.stringify(db,null,4),"utf-8")
+       fs.writeFileSync("./badCities.json",JSON.stringify(badCities,null,4),"utf-8")
     }
-    fs.writeFileSync("./db.json",JSON.stringify(db,null,4),"utf-8")
+    
 })
 
 
